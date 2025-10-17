b0561478e10e1c44d6a63100af3eaf692a5167ba
方婕mini <fangjiemini@fangjieminideMac-mini.local>
2025-10-17 17:08:54 +0800

    Feat: 新增 nav 次選單

--- apps/web/src/app/modules/_layout/pages/default-container/default-container.component.ts
+++ apps/web/src/app/modules/_layout/pages/default-container/default-container.component.ts
@@ -3,6 +3,7 @@ import { Component, Injector, OnInit, ChangeDetectorRef, ViewChild, AfterViewIni
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSidenav } from '@angular/material/sidenav';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 // Custom packages
 import { BBDBaseComponent } from '@core/shared';
@@ -16,10 +17,54 @@ import { BBDBaseComponent } from '@core/shared';
 export class DefaultContainerComponent extends BBDBaseComponent implements OnInit {
   currentUrl = '';
   @ViewChild('snav') snav!: MatSidenav;
+  activeMenu: string | null = null;
+  isMenuOpen = false;
+
+  navList = [
+    {
+      label: '關於本會',
+      children: [
+        { label: '本會簡介', link: '/about/introduction' },
+        { label: '組織章程', link: '/about/organization' },
+        { label: '理監事名單', link: '/about/history' },
+        { label: '委員會組織', link: '/about/honorary' },
+      ]
+    },
+    {
+      label: '最新消息',
+      link: '/'
+    },
+    {
+      label: '學術活動',
+      children: [
+        { label: '國內外研討會', link: '/about/introduction' },
+        { label: '教育積分申請', link: '/about/organization' },
+        { label: '積分申請進度', link: '/about/history' },
+        { label: '活動報名查詢', link: '/about/honorary' },
+        { label: '活動花絮', link: '/about/honorary' },
+      ]
+    },
+    {
+      label: '認證專區',
+      children: [
+        { label: '訓練醫院 / 機構申請', link: '/about/introduction' },
+        { label: '合格訓練醫院', link: '/about/organization' },
+        { label: '合格訓練機構', link: '/about/history' },
+      ]
+    },
+    {
+      label: '投稿專區',
+      children: [
+        { label: '年會投稿', link: '/about/introduction' },
+        { label: '雜誌投稿', link: '/about/organization' },
+        { label: '投稿進度查詢', link: '/about/history' },
+      ]
+    }
+  ];
 
   constructor(protected override injector: Injector,
-    changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
-    private _router: Router,
+    //changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
+    //private _router: Router,
     private activatedRoute: ActivatedRoute) {
     super(injector);
   }
@@ -28,14 +73,24 @@ export class DefaultContainerComponent extends BBDBaseComponent implements OnIni
     console.log();
   }
 
-  toggleMenu() {
+  toggleSideMenu() {
     this.snav.toggle(); // 切換側邊菜單
   }
 
+  toggleNavbarMenu(label: string) {
+    this.activeMenu = this.activeMenu === label ? null : label;
+  }
+
+  toggleSingleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  /*
   getUrlWithoutParams(url: string): string {
     if (!url) {
       return url;
     }
     return new URL(url, location.origin)?.pathname;
   }
+    */
 }
--- apps/web/src/app/shared/components/glass-bg/glass-bg.component.ts
+++ apps/web/src/app/shared/components/glass-bg/glass-bg.component.ts
@@ -8,7 +8,6 @@ import {
 @Component({
   selector: 'web-glass-bg',
   templateUrl: './glass-bg.component.html',
-  styleUrls: ['./glass-bg.component.scss']
 })
 export class GlassBgComponent extends BBDBaseComponent implements OnInit {
 //  @Input() routerLink: string | any[] = '';
