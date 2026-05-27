4fe463751cac69e4a4143364705e14de8c7c2f98
FangjieHung <j87345@gmail.com>
2026-05-27 10:56:16 +0800

    feat: implement faq-page component

--- apps/web/src/app/modules/explanation/pages/faq-page/faq-page.component.ts
+++ apps/web/src/app/modules/explanation/pages/faq-page/faq-page.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Injector, signal } from '@angular/core';
+import { BBDBaseComponent } from '@core/shared';
+import { SEOServ } from '@core/services';
+import { CustomizationReferenceService } from '../../../../shared/services/customization-reference.service';
 
 @Component({
   selector: 'app-faq-page',
   templateUrl: './faq-page.component.html',
   styleUrls: ['./faq-page.component.scss'],
 })
-export class FaqPageComponent {}
+export class FaqPageComponent extends BBDBaseComponent {
+  private readonly refService = this.injector.get(CustomizationReferenceService);
+  private readonly seoServ = this.injector.get(SEOServ);
+
+  readonly faqGroups = signal(this.refService.faqGroups);
+
+  constructor(protected override injector: Injector) {
+    super(injector);
+    this._updateSeo();
+  }
+
+  private _updateSeo(): void {
+    this.seoServ.updateMetaTags(
+      '常見問題 | 富華澐光',
+      `${this.appEnv.siteServer}/explanation/faq`,
+      `${this.appEnv.siteServer}/assets/image/og/tslmai.png`,
+      '客變常見問題與解答'
+    );
+    this.seoServ.updateCanonical(`${this.appEnv.siteServer}/explanation/faq`);
+  }
+}
