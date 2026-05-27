3b22f7376959a962670a1c258fa5c9f58705020e
FangjieHung <j87345@gmail.com>
2026-05-27 11:05:09 +0800

    feat: implement equipment-page component

--- apps/web/src/app/modules/explanation/pages/equipment-page/equipment-page.component.ts
+++ apps/web/src/app/modules/explanation/pages/equipment-page/equipment-page.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Injector, signal } from '@angular/core';
+import { BBDBaseComponent } from '@core/shared';
+import { SEOServ } from '@core/services';
+import { CustomizationReferenceService } from '../../../../shared/services/customization-reference.service';
 
 @Component({
   selector: 'app-equipment-page',
   templateUrl: './equipment-page.component.html',
   styleUrls: ['./equipment-page.component.scss'],
 })
-export class EquipmentPageComponent {}
+export class EquipmentPageComponent extends BBDBaseComponent {
+  private readonly refService = this.injector.get(CustomizationReferenceService);
+  private readonly seoServ = this.injector.get(SEOServ);
+
+  readonly equipmentByCategory = signal(this.refService.getEquipmentByCategory());
+
+  constructor(protected override injector: Injector) {
+    super(injector);
+    this._updateSeo();
+  }
+
+  private _updateSeo(): void {
+    this.seoServ.updateMetaTags(
+      '設備表 | 富華澐光',
+      `${this.appEnv.siteServer}/explanation/equipment`,
+      `${this.appEnv.siteServer}/assets/image/og/tslmai.png`,
+      '標準設備與升級選項'
+    );
+    this.seoServ.updateCanonical(`${this.appEnv.siteServer}/explanation/equipment`);
+  }
+}
